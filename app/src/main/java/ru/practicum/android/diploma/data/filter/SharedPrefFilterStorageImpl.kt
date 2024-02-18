1ef5cffdf636222dc08cbce8fdd9829124f6d9cb
looee <transistor42b@yandex.ru>
2024-02-18 21:55:24 +0300

    task_9. Создал класс, в котором будет происходить сохранение фильтров в SharedPreferences и получение их оттуда

---
+++ app/src/main/java/ru/practicum/android/diploma/data/filter/SharedPrefFilterStorageImpl.kt
@@ -0,0 +1,7 @@
+package ru.practicum.android.diploma.data.filter
+
+import android.content.SharedPreferences
+
+class SharedPrefFilterStorageImpl(
+    private val sharedPreferences: SharedPreferences
+) : FilterStorage
