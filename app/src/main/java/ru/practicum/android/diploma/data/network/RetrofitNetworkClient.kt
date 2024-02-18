2513079aabd039417c37db830dfaf0522efb09d9
Daria Popova <dassenka@yandex.ru>
2024-02-18 16:46:53 +0300

    fixed3 according comments of PR checker("detekt")

--- app/src/main/java/ru/practicum/android/diploma/data/network/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/RetrofitNetworkClient.kt
@@ -11,20 +11,22 @@ import ru.practicum.android.diploma.data.dto.RequestVacanciesListSearch
 import ru.practicum.android.diploma.data.dto.RequestVacancySearch
 import ru.practicum.android.diploma.data.dto.Response
 
-class RetrofitNetworkClient
-    (
+class RetrofitNetworkClient(
+
     private val hhApiService: HHApiService,
     private val context: Context,
-) : NetworkClient {
+
+    ) : NetworkClient {
 
     override suspend fun doRequest(dto: Any): Response<out Any> {
+
         if (!isConnected()) {
             return Response<Any>().apply { resultCode = NO_INTERNET_CONNECTION }
         }
+
         if (dto !is RequestVacanciesListSearch
             && dto !is RequestVacancySearch
-            && dto !is RequestSimilarVacancySearch
-        // && dto !is RequestGetFiltersValues
+            && dto !is RequestSimilarVacancySearch // && dto !is RequestGetFiltersValues
         ) {
             return Response<Any>().apply { resultCode = BAD_REQUEST }
         }
@@ -70,10 +72,13 @@ class RetrofitNetworkClient
         val capabilities =
             connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
         if (capabilities != null) {
-            when {
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> return true
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> return true
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> return true
+            return when {
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> {
+                    false
+                }
             }
         }
         return false
