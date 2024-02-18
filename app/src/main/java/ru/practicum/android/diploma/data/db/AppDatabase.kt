3711265d698f0b522752ad4e51f4b677e3e96759
Daria Popova <dassenka@yandex.ru>
2024-02-18 23:20:22 +0300

    add abstract class AppDatabase

---
+++ app/src/main/java/ru/practicum/android/diploma/data/db/AppDatabase.kt
@@ -0,0 +1,9 @@
+package ru.practicum.android.diploma.data.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import ru.practicum.android.diploma.data.db.entity.FavoriteVacancyEntity
+
+@Database(version = 1, entities = [FavoriteVacancyEntity::class])
+abstract class AppDatabase : RoomDatabase() {
+}
