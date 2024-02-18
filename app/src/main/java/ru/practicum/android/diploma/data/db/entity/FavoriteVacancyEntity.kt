35f336836e4bc423bf5161c5133ebaf43a614eef
Daria Popova <dassenka@yandex.ru>
2024-02-18 23:14:39 +0300

    add FavoriteVacancyEntity

---
+++ app/src/main/java/ru/practicum/android/diploma/data/db/entity/FavoriteVacancyEntity.kt
@@ -0,0 +1,35 @@
+package ru.practicum.android.diploma.data.db.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "fav_vacancy_table")
+data class FavoriteVacancyEntity(
+    @PrimaryKey
+    val id: String,
+    val areaId: String,
+    val areaName: String,
+    val name: String,
+    val employerId: String,
+    val employerName: String,
+    val employerLogoUrls: String,
+    val salaryCurrency: String,
+    val salaryFrom: Int,
+    val salaryGross: Boolean,
+    val salaryTo: Int,
+    val contactsEmail: String,
+    val contactsName: String,
+    val contactsPhonesCity: String,
+    val contactsPhonesComment: String,
+    val contactsPhonesCountry: String,
+    val contactsPhonesNumber: String,
+    val comment: String,
+    val description: String,
+    val employment: String,
+    val experience: String,
+    val keySkills: List<String>,
+    val alternateUrl: String,
+    val schedule: String,
+)
+
+
