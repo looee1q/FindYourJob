11496f5f08473c286a401737e66238fb9b223613
Pavel Ivanov <PAleksandrIvanov@sberbank.ru>
2024-02-20 18:09:34 +0300

    task_6: внедрение зависимостей

---
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -0,0 +1,50 @@
+package ru.practicum.android.diploma.di
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.net.ConnectivityManager
+import androidx.room.Room
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.data.NetworkClient
+import ru.practicum.android.diploma.data.db.AppDatabase
+import ru.practicum.android.diploma.data.filter.FilterStorage
+import ru.practicum.android.diploma.data.filter.SharedPrefFilterStorageImpl
+import ru.practicum.android.diploma.data.network.HHApiService
+import ru.practicum.android.diploma.data.network.RetrofitNetworkClient
+
+val dataModule = module {
+
+    single<SharedPreferences> {
+        androidContext().getSharedPreferences("FIND_YOUR_JOB_PREFERENCES", Context.MODE_PRIVATE)
+    }
+
+    single {
+        Room
+            .databaseBuilder(androidContext(), AppDatabase::class.java, "database.db")
+            .build()
+    }
+
+    single<HHApiService> {
+        Retrofit
+            .Builder()
+            .baseUrl("https://api.hh.ru/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(HHApiService::class.java)
+    }
+
+    single<ConnectivityManager> {
+        androidContext().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    }
+
+    single<FilterStorage> {
+        SharedPrefFilterStorageImpl(sharedPreferences = get())
+    }
+
+    single<NetworkClient> {
+        RetrofitNetworkClient(hhApiService = get(), context = androidContext(), connectivityManager = get())
+    }
+}
