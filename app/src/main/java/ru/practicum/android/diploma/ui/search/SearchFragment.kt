e46d08f08b8cf5567c06a822105dc352313e4f63
Daria Popova <dassenka@yandex.ru>
2024-02-23 23:04:44 +0300

    add addTextChangedListener in SearchFragment

--- app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
@@ -1,9 +1,13 @@
 package ru.practicum.android.diploma.ui.search
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
 import ru.practicum.android.diploma.ui.fragment.BindingFragment
 
@@ -18,5 +22,45 @@ class SearchFragment : BindingFragment<FragmentSearchBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.icClose.setOnClickListener {
+            binding.InputEditText.setText("")
+            hideKeyBoard()
+            binding.placeHolderError.visibility = View.GONE
+            binding.progressBar.visibility = View.GONE
+            //adapter.track.clear()
+            //adapter.notifyDataSetChanged()
+        }
+
+        binding.InputEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binding.placeHolderError.visibility = View.GONE
+                //binding.icClose.visibility = clearButtonVisibility(s)
+                //viewModel.searchDebounce(changedText = s?.toString() ?: inputText, false)
+
+                if (binding.InputEditText.hasFocus() && s?.isEmpty() == true) {
+                    binding.recyclerViewFoundVacancies.visibility = View.GONE
+                    binding.icClose.visibility = View.GONE
+                } else {
+                    binding.recyclerViewFoundVacancies.visibility = View.VISIBLE
+                    binding.icClose.visibility = View.VISIBLE
+
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
+    }
+
+    private fun hideKeyBoard() {
+        val inputMethodManager =
+            requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(binding.InputEditText.windowToken, 0)
     }
 }
