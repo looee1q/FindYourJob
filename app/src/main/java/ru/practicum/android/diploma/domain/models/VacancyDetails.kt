00af6bfff551df9d4cc956a509e20ab1b17be80a
Pavel Ivanov <PAleksandrIvanov@sberbank.ru>
2024-02-26 16:15:34 +0300

    task_28: выполнение запроса деталей вакансии

--- app/src/main/java/ru/practicum/android/diploma/domain/models/VacancyDetails.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/VacancyDetails.kt
@@ -6,6 +6,9 @@ package ru.practicum.android.diploma.domain.models
  * @param id идентификатор вакансии
  * @param alternateUrl ссылка на представление вакансии на сайте
  * @param areaName название региона
+ * @param contactEmail электронная почта контакта
+ * @param contactName имя контакта
+ * @param contactPhone телефон контакта
  * @param description описание в html, не менее 200 символов
  * @param employerName название компании
  * @param employment тип занятости
@@ -21,7 +24,11 @@ data class VacancyDetails(
     val id: String,
     val alternateUrl: String,
     val areaName: String,
+    val contactEmail: String,
+    val contactName: String,
+    val contactPhone: String,
     val description: String,
+    val employerLogoUrl: String,
     val employerName: String,
     val employment: String,
     val experience: String,
