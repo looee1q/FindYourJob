5d17007a5a0ec0a3dcc5e87b3ca4bf01833611b3
Daria Popova <dassenka@yandex.ru>
2024-03-07 19:18:30 +0300

    add logic for selection region

--- app/src/main/java/ru/practicum/android/diploma/presentation/selections/area/AreaSelectionViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/selections/area/AreaSelectionViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import ru.practicum.android.diploma.domain.api.FilterSearchInteractor
 import ru.practicum.android.diploma.domain.models.Country
 import ru.practicum.android.diploma.domain.models.FilterParameters
+import ru.practicum.android.diploma.domain.models.Region
 
 class AreaSelectionViewModel(private val filterSearchInteractor: FilterSearchInteractor) : ViewModel() {
 
@@ -12,9 +13,29 @@ class AreaSelectionViewModel(private val filterSearchInteractor: FilterSearchInt
         savedCountryParameters.let { filterSearchInteractor.saveFilterParameters(it) }
     }
 
+    fun saveRegionToSharedPref(regionId: String, regionName: String) {
+        val savedCountryParameters = FilterParameters(idRegion = regionId, nameRegion = regionName)
+        savedCountryParameters.let { filterSearchInteractor.saveFilterParameters(it) }
+    }
+
+    fun saveCountryAndRegionToSharedPref(countryId: String, countryName: String, regionId: String, regionName: String) {
+        val savedCountryParameters = FilterParameters(
+            idCountry = countryId,
+            nameCountry = countryName,
+            idRegion = regionId,
+            nameRegion = regionName
+        )
+        savedCountryParameters.let { filterSearchInteractor.saveFilterParameters(it) }
+    }
+
     fun getCountryFromSharedPref(): Country {
         val filterParameters = filterSearchInteractor.getFilterParameters()
         return Country(id = filterParameters?.idCountry ?: "", name = filterParameters?.nameCountry ?: "")
+    }
+
+    fun getRegionFromSharedPref(): Region {
+        val filterParameters = filterSearchInteractor.getFilterParameters()
+        return Region(id = filterParameters?.idRegion ?: "", name = filterParameters?.nameRegion ?: "", parentId = "")
     }
 
 }
--- app/src/main/java/ru/practicum/android/diploma/ui/selections/area/AreaSelectionFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/selections/area/AreaSelectionFragment.kt
@@ -5,12 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
+import androidx.core.view.isVisible
 import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentAreaSelectionBinding
 import ru.practicum.android.diploma.domain.models.Country
+import ru.practicum.android.diploma.domain.models.Region
 import ru.practicum.android.diploma.presentation.selections.area.AreaSelectionViewModel
 import ru.practicum.android.diploma.ui.fragment.BindingFragment
 import ru.practicum.android.diploma.ui.selections.country.CountrySelectionFragment
@@ -23,8 +25,13 @@ class AreaSelectionFragment : BindingFragment<FragmentAreaSelectionBinding>() {
     private var countryFromSharedPref: Country? = null
     private var countryNameFromSharedPref: String = ""
     private var countryIdFromSharedPref: String = ""
+    private var regionFromSharedPref: Region? = null
+    private var regionNameFromSharedPref: String = ""
+    private var regionIdFromSharedPref: String = ""
     private var countryName: String = ""
     private var countryId: String = ""
+    private var regionName: String = ""
+    private var regionId: String = ""
 
     override fun createBinding(
         inflater: LayoutInflater,
@@ -36,42 +43,44 @@ class AreaSelectionFragment : BindingFragment<FragmentAreaSelectionBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        countryFromSharedPref = viewModel.getCountryFromSharedPref()
-        countryNameFromSharedPref = countryFromSharedPref?.name ?: ""
+        installFromSharedPref()
+        checkCountry()
+        checkRegion()
 
         setFragmentResultListener(CountrySelectionFragment.REQUEST_KEY) { key, bundle ->
             countryName = bundle.getString("NAME") ?: ""
             countryId = bundle.getString("ID") ?: ""
-            checkFragment()
+            checkCountry()
         }
 
-        checkFragment()
+        setFragmentResultListener(RegionSelectionFragment.REQUEST_KEY_REGION) { key, bundle ->
+            regionName = bundle.getString("NAME") ?: ""
+            regionId = bundle.getString("ID") ?: ""
+            checkRegion()
+        }
 
         binding.btnBack.setOnClickListener {
             findNavController().navigateUp()
         }
 
         binding.countryLayout.setOnClickListener {
-            findNavController().navigate(
-                R.id.action_areaSelectionFragment_to_countrySelectionFragment
-            )
+            findNavController().navigate(R.id.action_areaSelectionFragment_to_countrySelectionFragment)
         }
 
         binding.regionLayout.setOnClickListener {
-            findNavController().navigate(
-                R.id.action_areaSelectionFragment_to_regionSelectionFragment,
-                RegionSelectionFragment.createArgs("40")
-            )
+            openRegionSelectionFragment()
         }
 
         binding.icArrowForwardCountry.setOnClickListener {
-            setEmptyCountry()
-            viewModel.saveCountryToSharedPref(countryId, countryName)
+            clearCountry()
+        }
+
+        binding.icArrowForwardRegion.setOnClickListener {
+            clearRegion()
         }
 
         binding.buttonSelect.setOnClickListener {
-            viewModel.saveCountryToSharedPref(countryId, countryName)
-            findNavController().navigateUp()
+            saveCountryAndRegion()
         }
 
         requireActivity().onBackPressedDispatcher.addCallback(
@@ -84,13 +93,32 @@ class AreaSelectionFragment : BindingFragment<FragmentAreaSelectionBinding>() {
         )
     }
 
-    private fun checkFragment() {
+    private fun installFromSharedPref() {
+        countryFromSharedPref = viewModel.getCountryFromSharedPref()
+        countryNameFromSharedPref = countryFromSharedPref?.name ?: ""
+        countryIdFromSharedPref = countryFromSharedPref?.id ?: ""
+        regionFromSharedPref = viewModel.getRegionFromSharedPref()
+        regionNameFromSharedPref = regionFromSharedPref?.name ?: ""
+        regionIdFromSharedPref = regionFromSharedPref?.id ?: ""
+    }
+
+    private fun checkCountry() {
         if (countryNameFromSharedPref.isNotEmpty() && countryName.isEmpty()) {
             countryFromSharedPref?.let { setSelectedCountry(it.name) }
         } else if (countryNameFromSharedPref.isEmpty() && countryName.isEmpty()) {
             setEmptyCountry()
         } else if (countryName.isNotEmpty()) {
             setSelectedCountry(countryName)
+        }
+    }
+
+    private fun checkRegion() {
+        if (regionNameFromSharedPref.isNotEmpty() && regionName.isEmpty()) {
+            regionFromSharedPref?.let { setSelectedRegion(it.name) }
+        } else if (regionNameFromSharedPref.isEmpty() && regionName.isEmpty()) {
+            setEmptyRegion()
+        } else if (regionName.isNotEmpty()) {
+            setSelectedRegion(regionName)
         }
     }
 
@@ -114,12 +142,74 @@ class AreaSelectionFragment : BindingFragment<FragmentAreaSelectionBinding>() {
         showButtonSelect()
     }
 
+    private fun setSelectedRegion(countryName: String) {
+        binding.RegionNameTitle.visibility = View.GONE
+        binding.RegionSupportText.visibility = View.VISIBLE
+        binding.icArrowForwardRegion.isSelected = true
+        binding.RegionName.visibility = View.VISIBLE
+        binding.RegionName.text = countryName
+        showButtonSelect()
+    }
+
+    private fun setEmptyRegion() {
+        binding.RegionNameTitle.visibility = View.VISIBLE
+        binding.RegionSupportText.visibility = View.GONE
+        binding.icArrowForwardRegion.isSelected = false
+        binding.RegionName.visibility = View.GONE
+        binding.RegionName.text = ""
+        regionName = ""
+        regionId = ""
+        showButtonSelect()
+    }
+
+    private fun clearRegion() {
+        setEmptyRegion()
+        viewModel.saveRegionToSharedPref(regionId, regionName)
+    }
+
+    private fun clearCountry() {
+        setEmptyCountry()
+        viewModel.saveCountryToSharedPref(countryId, countryName)
+    }
+
     private fun showButtonSelect() {
-        if (countryName.isEmpty() && countryNameFromSharedPref.isEmpty()) {
+        if (binding.RegionNameTitle.isVisible && binding.countryNameTitle.isVisible) {
             binding.buttonSelect.visibility = View.GONE
         } else {
             binding.buttonSelect.visibility = View.VISIBLE
         }
+    }
+
+    private fun openRegionSelectionFragment() {
+        findNavController().navigate(
+            R.id.action_areaSelectionFragment_to_regionSelectionFragment,
+            if (countryId.isNotEmpty()) {
+                RegionSelectionFragment.createArgs(countryId)
+            } else {
+                RegionSelectionFragment.createArgs(countryIdFromSharedPref)
+            }
+        )
+    }
+
+    private fun saveCountryAndRegion() {
+        if (countryId.isNotEmpty()) {
+            viewModel.saveCountryAndRegionToSharedPref(countryId, countryName, regionId, regionName)
+        } else if (countryId.isEmpty() && regionId.isNotEmpty()) {
+            viewModel.saveCountryAndRegionToSharedPref(
+                countryIdFromSharedPref,
+                countryNameFromSharedPref,
+                regionId,
+                regionName
+            )
+        } else {
+            viewModel.saveCountryAndRegionToSharedPref(
+                countryIdFromSharedPref,
+                countryNameFromSharedPref,
+                regionIdFromSharedPref,
+                regionNameFromSharedPref
+            )
+        }
+        findNavController().navigateUp()
     }
 
 }
